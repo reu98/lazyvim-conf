fe0504adb819d8e7034af1db009edfd4232cf7b6
Hoàng Nguyễn <reu98.dev@gmail.com>
2024-12-03 23:24:50 +0700

    feat: preview image

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.prisma" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
+    { import = "lazyvim.plugins.extras.ui.smear-cursor" },
     -- import/override with your plugins
     { import = "plugins" },
   },
