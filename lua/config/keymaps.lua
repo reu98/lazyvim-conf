3264ec3e7f0854e6f68544d019a1745e140109eb
Lưu Tiến Luận <luanluuhaui@gmail.com>
2024-12-05 10:52:52 +0700

    [chore] ltl configs
    # Conflicts:
    #	lazy-lock.json
    #	lua/config/keymaps.lua
    #	lua/config/lazy.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,38 @@
 
 vim.keymap.set("v", "<S-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
 vim.keymap.set("v", "<S-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+-- codeium
+-- https://github.com/Exafunction/codeium.vim?tab=readme-ov-file
+return {
+  "Exafunction/codeium.vim",
+  config = function()
+    -- Change '<C-g>' here to any keycode you like.
+    vim.keymap.set("i", "<C-g>", function()
+      return vim.fn["codeium#Accept"]()
+    end, { expr = true, silent = true })
+    vim.keymap.set("i", "<c-;>", function()
+      return vim.fn["codeium#CycleCompletions"](1)
+    end, { expr = true, silent = true })
+    vim.keymap.set("i", "<c-,>", function()
+      return vim.fn["codeium#CycleCompletions"](-1)
+    end, { expr = true, silent = true })
+    vim.keymap.set("i", "<c-x>", function()
+      return vim.fn["codeium#Clear"]()
+    end, { expr = true, silent = true })
+    vim.keymap.set("<leader>to", "<cmd>tabnew<CR>", function()
+      return vim.fn["Open new tab"]()
+    end, { expr = true, silent = true })
+    vim.keymap.set("<leader>tx", "<cmd>tabclose<CR>", function()
+      return vim.fn["Close new tab"]()
+    end, { expr = true, silent = true })
+    vim.keymap.set("<leader>tn", "<cmd>tabn<CR>", function()
+      return vim.fn["Goto next tab"]()
+    end, { expr = true, silent = true })
+    vim.keymap.set("<leader>tp", "<cmd>tabp<CR>", function()
+      return vim.fn["Goto previous tab"]()
+    end, { expr = true, silent = true })
+    vim.keymap.set("<leader>tf", "<cmd>tabnew %<CR>", function()
+      return vim.fn["Open current buffer in new tab"]()
+    end, { expr = true, silent = true })
+  end,
+}
