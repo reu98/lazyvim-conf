8cfc0881c771d28d34af236ea82ff334c221ac2d
Hoàng Nguyễn <reu98.dev@gmail.com>
2024-12-11 19:13:52 +0700

    feat: add emoji

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,4 +1,15 @@
 return {
   "stevearc/conform.nvim",
-  opts = {},
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      go = { "goimports", "gofumpt" },
+    },
+    formatters = {
+      prettier = {
+        prepend_args = { "--end-of-line auto", "--tab-width 4", "--bracket-same-line" },
+      },
+    },
+  },
 }
