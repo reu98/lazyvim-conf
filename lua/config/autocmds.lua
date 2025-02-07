3f0d9eb7357f409ac99da9931e310819491129a4
Hoàng Nguyễn <reu98.dev@gmail.com>
2025-02-07 13:04:06 +0700

    feat: add spell check

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local function debounce(ms, fn)
+  local timer = vim.uv.new_timer()
+  return function(...)
+    local argv = { ... }
+    timer:start(ms, 0, function()
+      timer:stop()
+      vim.schedule_wrap(fn)(unpack(argv))
+    end)
+  end
+end
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+  callback = function()
+    local lint = require("lint")
+
+    debounce(100, lint.try_lint())
+  end,
+})
